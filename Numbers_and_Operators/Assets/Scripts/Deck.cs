252834a57a90ec812b4a8857c31a38cf1ba861de
takaya0 <ytk.koizumi@gmail.com>
2022-07-25 01:12:23 +0900

    add Deck System

---
+++ Numbers_and_Operators/Assets/Scripts/Deck.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class Deck{
+    public string deckName;
+    public Dictionary<string, int> oepratorsDeck;
+    public Dictionary<string, int> numbersDeck;
+
+}
